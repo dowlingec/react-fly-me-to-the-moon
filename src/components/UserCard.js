ef5583c055af3d3fe8d226bad3d4067b1a279fa2
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-11 10:25:47 -0400

    added accesibility feature to UserCard

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,7 +1,7 @@
 function UserCard({user}) {
     return (
         <div className="user-card">
-            <img src={user.image_url} alt="user profile"></img>
+            <img src={user.image_url} alt={'Avatar representing ' + user.username}></img>
             <h2>{user.username}</h2>
         </div>
     )
