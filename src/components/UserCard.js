80412c721ce88d29335eb094823fe6a3464103d7
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-12 11:04:06 -0400

    user-card click handle a created

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,6 +1,15 @@
+import {useState} from 'react'
+
 function UserCard({user}) {
+    const [userId, setUserId] = useState("")
+
+    const handleClick = () => {
+        setUserId(1)
+    }
+    console.log('Current user', userId)
+
     return (
-        <div className="user-card" onClick={() => alert('hi! I was clicked')}>
+        <div className="user-card" onClick={handleClick}>
             <img src={user.image_url} alt={'Avatar representing ' + user.username}></img>
             <h2>{user.username}</h2>
         </div>
