5f1c27d682f1db243f5c90f06cec700f014da7a8
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-13 12:05:03 -0400

    About renders correctly

--- src/components/About.js
+++ src/components/About.js
@@ -11,13 +11,21 @@ function About() {
         })()
     }, [])
 
+    console.log("About stuffs", chemicals)
     return(
         <div>
             <h1>About Us</h1>
             <p>We belive the best cure is prevention, so have a little read about our brain chemicals and see what you can do to prevent (or at least lessen) the stress of everyday life by helping your brain make these wonderful chemicals</p>
-            <div>
-                {/* <h1>{chemicals[0]}</h1> */}
-            </div>
+            
+            {chemicals.map((el)=> {
+            return (
+                <div className="chemical-card">
+                    <h1>{el.name}</h1>
+                    <h3>{el.description}</h3>
+                </div>
+            )
+        })}
+            
 
         </div>
     )
