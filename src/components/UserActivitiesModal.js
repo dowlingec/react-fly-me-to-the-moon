7e72f5af7ca64e97efa31ea49f4f3ed1f5b608e8
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-12 23:41:02 -0400

    info persisting, bedtime

--- src/components/UserActivitiesModal.js
+++ src/components/UserActivitiesModal.js
@@ -5,7 +5,10 @@ function UserActivitiesModal({activity}) {
     return (
         <div>
             <h1>{activity.name}</h1>
-            <h3>Someone kill me</h3>
+            <p>Notes - {activity.notes}</p>
+            <h3>Takes around {activity.duration} mins</h3>
+            <h5>This is an {activity.location} activity.</h5>
+
         </div>
     )
 }
